d508e4107561e5221e516e829e3b312f8eb347ac
mcgrady00h <281130306@qq.com>
2017-11-28 01:15:46 +0800

    add put stone rule

--- GTP/utils.py
+++ GTP/utils.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
 # $File: utils.py
-# $Date: Fri Nov 17 10:2407 2017 +0800
+# $Date: Mon Nov 27 18:2755 2017 +0800
 # $Author: renyong15 © <mails.tsinghua.edu.cn>
 #
 
 WHITE = -1
-BLACK = +1
 EMPTY = 0
+BLACK = +1
+FILL = +2
+KO = +3
+UNKNOWN = +4
 
 PASS = (0,0)
 RESIGN = "resign"
+
+def another_color(color):
+    return color * -1
 
 
 
