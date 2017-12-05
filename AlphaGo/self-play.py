2d9d1ff9455401157b05b8179e6d1514c6cf3513
rtz19970824 <rtz19970824@gmail.com>
2017-12-05 23:42:18 +0800

    minor fixed

--- AlphaGo/self-play.py
+++ AlphaGo/self-play.py
@@ -1,5 +1,6 @@
 from game import Game
 from engine import GTPEngine
+import re
 
 g = Game()
 
@@ -12,15 +13,19 @@ black_pass = False
 white_pass = False
 while not (black_pass and white_pass):
     if num % 2 == 0:
-        res=e.run_cmd("genmove BLACK")[0]
+        res = e.run_cmd(str(num) + " genmove BLACK")
         num += 1
         print(res)
-        if res == (0,0):
+        if re.search("pass", res) is not None:
             black_pass = True
+        else:
+            black_pass = False
     else:
-        res = e.run_cmd("genmove WHITE")[0]
+        res = e.run_cmd(str(num) + " genmove WHITE")
         num += 1
         print(res)
-        if res == (0, 0):
+        if re.search("pass", res) is not None:
             white_pass = True
+        else:
+            white_pass = False
     g.show_board()
