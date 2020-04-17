fix formula error, add accuracy check

test/base/test_prioritized_replay_buffer.py
@@ -16,6 +16,8 @@ def test_replaybuffer(size=32, bufsize=15):
         obs_next, rew, done, info = env.step(a)
         buf.add(obs, a, rew, done, obs_next, info, np.random.randn()-0.5)
         obs = obs_next
+        assert np.isclose(np.sum((buf.weight/buf._weight_sum)[:buf._size]), 1,
+                          rtol=1e-12)
         data, indice = buf.sample(len(buf) // 2)
         if len(buf)//2 == 0:
             assert len(data) == len(buf)
@@ -23,7 +25,7 @@ def test_replaybuffer(size=32, bufsize=15):
             assert len(data) == len(buf)//2
         assert len(buf) == min(bufsize, i + 1), print(len(buf), i)
         assert np.isclose(buf._weight_sum, (buf.weight).sum())
-    data, indice = buf.sample(bufsize // 2)
+    data, indice = buf.sample(len(buf) // 2)
     buf.update_weight(indice, -data.weight/2)
     assert np.isclose(buf.weight[indice], np.power(
         np.abs(-data.weight/2), buf._alpha)).all()
@@ -31,5 +33,5 @@ def test_replaybuffer(size=32, bufsize=15):
 
 
 if __name__ == "__main__":
-    test_replaybuffer()
+    test_replaybuffer(233333, 200000)
     print("pass")

tianshou/data/buffer.py
@@ -267,6 +267,8 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         self._beta = beta  # importance sample soft coefficient
         self._weight_sum = 0.0
         self.weight = np.zeros(size)
+        self._amortization_freq = 1500
+        self._amortization_counter = 0
 
     def add(self, obs, act, rew, done, obs_next=0, info={}, weight=1.0):
         """Add a batch of data into replay buffer."""
@@ -275,6 +277,7 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         # we have to sacrifice some convenience for speed :(
         self._add_to_buffer('weight', np.abs(weight)**self._alpha)
         super().add(obs, act, rew, done, obs_next, info)
+        self._check_weight_sum()
 
     def sample(self, batch_size: int = 0, importance_sample: bool = True):
         """ Get a random sample from buffer with priority probability. \
@@ -300,16 +303,19 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         batch = self[indice]
         if importance_sample:
             impt_weight = Batch(
-                impt_weight=1/np.power(self._size*batch.weight, self._beta))
+                impt_weight=1/np.power(
+                    self._size*(batch.weight/self._weight_sum), self._beta))
             batch.append(impt_weight)
+        self._check_weight_sum()
         return batch, indice
 
     def reset(self):
+        self._amortization_counter = 0
         super().reset()
 
     def update_weight(self, indice, new_weight: np.ndarray):
         self._weight_sum += np.power(np.abs(new_weight), self._alpha).sum() \
-                         - self.weight[indice].sum()
+            - self.weight[indice].sum()
         self.weight[indice] = np.power(np.abs(new_weight), self._alpha)
 
     def __getitem__(self, index):
@@ -322,3 +328,10 @@ class PrioritizedReplayBuffer(ReplayBuffer):
             info=self.info[index],
             weight=self.weight[index]
         )
+
+    def _check_weight_sum(self):
+        # keep a accurate _weight_sum
+        self._amortization_counter += 1
+        if self._amortization_counter % self._amortization_freq == 0:
+            self._weight_sum = np.sum(self.weight)
+            self._amortization_counter = 0