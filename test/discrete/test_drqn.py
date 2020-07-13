0468f96c6be062b4340a833addb4cb902a56c896
Trinkle23897 <463003665@qq.com>
2020-07-13 16:40:03 +0800

    test commit

--- test/discrete/test_drqn.py
+++ test/discrete/test_drqn.py
@@ -16,7 +16,7 @@ from tianshou.utils.net.common import Recurrent
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', type=str, default='CartPole-v0')
-    parser.add_argument('--seed', type=int, default=1626)
+    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--eps-test', type=float, default=0.05)
     parser.add_argument('--eps-train', type=float, default=0.1)
     parser.add_argument('--buffer-size', type=int, default=20000)
