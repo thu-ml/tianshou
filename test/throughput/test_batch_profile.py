f6e6e8a0d21174b2f404a2e3b1340f09ea8fa7ab
ChenHuayu <308604256@qq.com>
2020-07-17 22:01:40 +0800

    *split all batch profile test. *delete print()

--- test/throughput/test_batch_profile.py
+++ test/throughput/test_batch_profile.py
@@ -59,82 +59,62 @@ def data():
 
 
 def test_init(data):
-    init_time = time.time()
+    """Test Batch __init__()."""
     for _ in np.arange(10):
         _ = Batch(data['batch_set'])
-    init_time = time.time() - init_time
-    print("batch init time {:.3f} s".format(init_time))
 
 
-def test_get(data):
-    """Test get with item&attr."""
-    getitem_time = time.time()
+def test_get_item(data):
+    """Test get with item."""
     for _ in np.arange(1e5):
         _ = data['batch3'][data['indexs']]
-    getitem_time = time.time() - getitem_time
-    print("batch getitem time {:.3f} s".format(getitem_time))
 
-    getattr_time = time.time()
+
+def test_get_attr(data):
+    """Test get with attr."""
     for _ in np.arange(1e6):
         data['batch3'].get('obs')
         data['batch3'].get('reward')
         _, _ = data['batch3'].obs, data['batch3'].reward
-    getattr_time = time.time() - getattr_time
-    print("batch getattr time {:.3f} s".format(getattr_time))
 
 
-def test_set(data):
-    """Test set with item&attr."""
-    setitem_time = time.time()
+def test_set_item(data):
+    """Test set with item."""
     for _ in np.arange(1e4):
         data['batch3'][data['indexs']] = data['slice_dict']
-    setitem_time = time.time() - setitem_time
-    print("batch setitem time {:.3f} s".format(setitem_time))
 
-    setattr_time = time.time()
+
+def test_set_attr(data):
+    """Test set with attr."""
     for _ in np.arange(1e4):
         data['batch3'].c = np.arange(1e3)
         data['batch3'].obs = data['dict_set']
-    setattr_time = time.time() - setattr_time
-    print("batch setattr time {:.3f} s".format(setattr_time))
 
 
 def test_numpy_torch_convert(data):
     """Test conversion between numpy and torch."""
-    convert_time = time.time()
     for _ in np.arange(1e5):
         data['batch4'].to_torch()
         data['batch4'].to_numpy()
-    convert_time = time.time() - convert_time
-    print("batch numpy torch convert time {:.3f} s".format(convert_time))
 
 
 def test_pickle(data):
-    pickle_time = time.time()
     for _ in np.arange(1e4):
         pickle.loads(pickle.dumps(data['batch4']))
-    pickle_time = time.time() - pickle_time
-    print("batch load pickle time {:.3f} s".format(pickle_time))
 
 
 def test_cat(data):
     """Test cat"""
-    cat_time = time.time()
     for _ in np.arange(1e4):
         Batch.cat((data['batch0'], data['batch0']))
         data['batch1'].cat_(data['batch0'])
-    cat_time = time.time() - cat_time
-    print("batch cat time {:.3f} s".format(cat_time))
 
 
 def test_stack(data):
     """Test stack"""
-    stack_time = time.time()
     for _ in np.arange(1e4):
         Batch.stack((data['batch0'], data['batch0']))
         data['batch2'].stack(data['batch0'])
-    stack_time = time.time() - stack_time
-    print("batch stack time {:.3f} s".format(stack_time))
 
 
 if __name__ == '__main__':
