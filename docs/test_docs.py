0f9dfc711da94d35bd7e749a5c0ca6723e9af49d
Trinkle23897 <463003665@qq.com>
2020-07-22 10:55:40 +0800

    filter a warning

--- docs/test_docs.py
+++ docs/test_docs.py
@@ -4,13 +4,17 @@ import os
 import shutil
 from subprocess import Popen, PIPE
 
+filter_warnings = ["WARNING: LaTeX command 'latex' cannot be run (needed "
+                   "for math display), check the imgmath_latex setting"]
+
 
 def main():
     if os.path.exists('_build'):
         shutil.rmtree('_build')
     p = Popen(['make', 'html'], stderr=PIPE)
     _, err = p.communicate()
-    err = [i for i in err.decode().splitlines() if 'WARNING' in i]
+    err = [i for i in err.decode().splitlines()
+           if 'WARNING' in i and i not in filter_warnings]
     assert len(err) == 0, '\n'.join(err)
 
 
