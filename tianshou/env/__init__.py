591467218838f2e33edaf2afd68109919f57f9d1
Trinkle23897 <463003665@qq.com>
2020-07-26 16:30:38 +0800

    change env dir structure

--- tianshou/env/__init__.py
+++ tianshou/env/__init__.py
@@ -1,6 +1,8 @@
-from tianshou.env.basevecenv import BaseVectorEnv
-from tianshou.env.vecenv import VectorEnv, SubprocVectorEnv, \
-    RayVectorEnv, AsyncVectorEnv
+from tianshou.env.vecenv.base import BaseVectorEnv
+from tianshou.env.vecenv.dummy import VectorEnv
+from tianshou.env.vecenv.subproc import SubprocVectorEnv
+from tianshou.env.vecenv.asyncenv import AsyncVectorEnv
+from tianshou.env.vecenv.rayenv import RayVectorEnv
 from tianshou.env.maenv import MultiAgentEnv
 
 __all__ = [
