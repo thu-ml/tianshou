1a36482a340fd38e1a64c82fb41b0cf164c05c52
yingchengyang <ycy17@mails.tsinghua.edu.cn>
2020-07-29 17:48:01 +0800

    modify the CNN network in DQN for Atari

--- tianshou/utils/net/discrete.py
+++ tianshou/utils/net/discrete.py
@@ -41,25 +41,47 @@ class Critic(nn.Module):
 class DQN(nn.Module):
     """For advanced usage (how to customize the network), please refer to
     :ref:`build_the_network`.
+
+    In paper "Human-level control through deep reinforcement learning"
+    url: https://www.nature.com/articles/nature14236
+    researchers use DQN to play Atari games
+    In this paper, researches contribute the network as below:
+    input size: 84*84, deal with m most recent frames and stacks them to
+                produce the input to the (m = 4)
+    First hidden convolution Layer: 32 filters with kernel size 8
+                                    and stride 4, then a rectifier nonlinearity
+    Second hidden convolution Layer: 64 filters of with kernel size 4
+                                    and stride 2, then a rectifier nonlinearity
+    Third hidden convolutional Layer: 64 filters of with kernel size 3
+                                    and stride 1, then a rectifier nonlinearity
+    Fourth hidden Fully-connected Layer: 512 rectifier units
+    Output Fully-connected Layer: linear layer with a single output for
+                                   each valid action
     """
 
     def __init__(self, h, w, action_shape, device='cpu'):
         super(DQN, self).__init__()
         self.device = device
 
-        self.conv1 = nn.Conv2d(4, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
 
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
+        def conv2d_layers_size_out(size,
+                                   kernel_size_1=8, stride_1=4,
+                                   kernel_size_2=4, stride_2=2,
+                                   kernel_size_3=3, stride_3=1):
+            size = conv2d_size_out(size, kernel_size_1, stride_1)
+            size = conv2d_size_out(size, kernel_size_2, stride_2)
+            size = conv2d_size_out(size, kernel_size_3, stride_3)
+            return size
+
+        convw = conv2d_layers_size_out(w)
+        convh = conv2d_layers_size_out(h)
+        linear_input_size = convw * convh * 64
         self.fc = nn.Linear(linear_input_size, 512)
         self.head = nn.Linear(512, action_shape)
 
@@ -68,8 +90,8 @@ class DQN(nn.Module):
         if not isinstance(x, torch.Tensor):
             x = torch.tensor(x, device=self.device, dtype=torch.float32)
         x = x.permute(0, 3, 1, 2)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
         x = self.fc(x.reshape(x.size(0), -1))
         return self.head(x), state
