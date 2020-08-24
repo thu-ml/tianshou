c3d2bad0270d918caa6dd0ec6c0c47d726bebf9c
Trinkle23897 <463003665@qq.com>
2020-08-24 09:31:49 +0800

    test_pg is too slow

--- test/discrete/test_pg.py
+++ test/discrete/test_pg.py
@@ -16,10 +16,10 @@ from tianshou.data import Collector, ReplayBuffer
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', type=str, default='CartPole-v0')
-    parser.add_argument('--seed', type=int, default=1626)
+    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--buffer-size', type=int, default=20000)
     parser.add_argument('--lr', type=float, default=1e-3)
-    parser.add_argument('--gamma', type=float, default=0.9)
+    parser.add_argument('--gamma', type=float, default=0.95)
     parser.add_argument('--epoch', type=int, default=10)
     parser.add_argument('--step-per-epoch', type=int, default=1000)
     parser.add_argument('--collect-per-step', type=int, default=10)
