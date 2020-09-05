df90377e07dd885bb01e610f7c2844cef8d56ad0
Trinkle23897 <463003665@qq.com>
2020-09-05 16:18:11 +0800

    fix pytest

--- test/modelbase/test_psrl.py
+++ test/modelbase/test_psrl.py
@@ -26,7 +26,7 @@ def get_args():
     parser.add_argument('--test-num', type=int, default=100)
     parser.add_argument('--logdir', type=str, default='log')
     parser.add_argument('--render', type=float, default=0.)
-    return parser.parse_args()
+    return parser.parse_known_args()[0]
 
 
 def test_psrl(args=get_args()):
