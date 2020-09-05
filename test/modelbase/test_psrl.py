554d81e2eae7d5a677cf96bbf2676b002cf45a19
Trinkle23897 <463003665@qq.com>
2020-09-05 16:06:01 +0800

    soft link

--- test/modelbase/test_psrl.py
+++ test/modelbase/test_psrl.py
@@ -1,15 +1,14 @@
+import gym
 import torch
 import pprint
 import argparse
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
-from tianshou.env import DummyVectorEnv, SubprocVectorEnv
+from tianshou.policy import PSRLPolicy
 from tianshou.trainer import offpolicy_trainer
 from tianshou.data import Collector, ReplayBuffer
-
-import gym
-from tianshou.policy import PSRLPolicy
+from tianshou.env import DummyVectorEnv, SubprocVectorEnv
 
 
 def get_args():
@@ -19,12 +18,12 @@ def get_args():
     parser.add_argument('--eps-test', type=float, default=0)
     parser.add_argument('--eps-train', type=float, default=0.1)
     parser.add_argument('--buffer-size', type=int, default=20000)
-    parser.add_argument('--epoch', type=int, default=5)
+    parser.add_argument('--epoch', type=int, default=20)
     parser.add_argument('--step-per-epoch', type=int, default=100)
-    parser.add_argument('--collect-per-step', type=int, default=100)
+    parser.add_argument('--collect-per-step', type=int, default=10)
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--training-num', type=int, default=8)
-    parser.add_argument('--test-num', type=int, default=1)
+    parser.add_argument('--test-num', type=int, default=100)
     parser.add_argument('--logdir', type=str, default='log')
     parser.add_argument('--render', type=float, default=0.)
     return parser.parse_args()
@@ -32,6 +31,9 @@ def get_args():
 
 def test_psrl(args=get_args()):
     env = gym.make(args.task)
+    if args.task == "NChain-v0":
+        env.spec.reward_threshold = 3650  # discribed in PSRL paper
+    print("reward threahold:", env.spec.reward_threshold)
     args.state_shape = env.observation_space.shape or env.observation_space.n
     args.action_shape = env.env.action_space.shape or env.env.action_space.n
     # train_envs = gym.make(args.task)
