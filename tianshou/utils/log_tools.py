625b7eff556d14c48e81c76f6759bc3f4c5018cd
rocknamx8 <kart527526877@hotmail.com>
2020-09-25 23:31:25 +0800

    support using different key to get different instance

--- tianshou/utils/log_tools.py
+++ tianshou/utils/log_tools.py
@@ -3,11 +3,25 @@ import threading
 
 
 class SummaryWriter(tensorboard.SummaryWriter):
+    """
+    A more convenient Summary Writer, you can get the same instance of summary
+    writer every where after you have create one.
+
+    """
     _mutex_lock = threading.Lock()
+    _default_key = None
 
     @classmethod
-    def get_instance(cls, *args, **kwargs):
+    def get_instance(cls, key: str = 'default', *args, **kwargs):
         with SummaryWriter._mutex_lock:
             if not hasattr(SummaryWriter, "_instance"):
-                SummaryWriter._instance = SummaryWriter(*args, **kwargs)
-        return SummaryWriter._instance
+                SummaryWriter._instance = {
+                    key: SummaryWriter(*args, **kwargs)
+                    }
+                SummaryWriter._default_key = key
+            elif key == 'default':
+                key = SummaryWriter._default_key
+            elif key not in SummaryWriter._instance.keys():
+                writer = SummaryWriter(*args, **kwargs)
+                SummaryWriter._instance.update({key: writer})
+        return SummaryWriter._instance[key]
