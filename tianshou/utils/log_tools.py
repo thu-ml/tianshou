c1e517d486589d4de3c70e79694754be972258e0
Trinkle23897 <trinkle23897@gmail.com>
2020-09-26 21:16:16 +0800

    polish

--- tianshou/utils/log_tools.py
+++ tianshou/utils/log_tools.py
@@ -1,27 +1,30 @@
 import threading
-from typing import Any
 from torch.utils import tensorboard
+from typing import Any, Dict, Optional
 
 
 class SummaryWriter(tensorboard.SummaryWriter):
     """A more convenient Summary Writer.
-    
+
     You can get the same instance of summary writer everywhere after you
     created one.
     """
     _mutex_lock = threading.Lock()
-    _default_key = None
+    _default_key: str
+    _instance: Optional[Dict[str, "SummaryWriter"]] = None
 
     @classmethod
-    def get_instance(cls, key: str = 'default', *args: Any, **kwargs: Any):
+    def get_instance(
+        cls, key: Optional[str] = None, *args: Any, **kwargs: Any
+    ) -> "SummaryWriter":
         with SummaryWriter._mutex_lock:
-            if not hasattr(SummaryWriter, "_instance"):
+            if key is None:
+                key = SummaryWriter._default_key
+            if SummaryWriter._instance is None:
                 SummaryWriter._instance = {
                     key: SummaryWriter(*args, **kwargs)
                 }
                 SummaryWriter._default_key = key
-            elif key == 'default':
-                key = SummaryWriter._default_key
             elif key not in SummaryWriter._instance.keys():
                 writer = SummaryWriter(*args, **kwargs)
                 SummaryWriter._instance.update({key: writer})
