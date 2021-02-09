38496cf4664e94f6798d3d8d5e220e47d964e826
chy <308604256@qq.com>
2021-02-09 16:51:35 +0800

    last fix

--- test/discrete/test_a2c_with_il.py
+++ test/discrete/test_a2c_with_il.py
@@ -113,7 +113,7 @@ def test_a2c_with_il(args=get_args()):
         env.spec.reward_threshold = 190  # lower the goal
     net = Net(args.state_shape, hidden_sizes=args.hidden_sizes,
               device=args.device)
-    net = Actor(net, args.action_shape).to(args.device)
+    net = Actor(net, args.action_shape, device=args.device).to(args.device)
     optim = torch.optim.Adam(net.parameters(), lr=args.il_lr)
     il_policy = ImitationPolicy(net, optim, mode='discrete')
     il_test_collector = Collector(
