e4d681ac0a9e1eed2ccecb0607784c4e35ddfcf9
Trinkle23897 <trinkle23897@gmail.com>
2021-02-22 16:59:50 +0800

    small update

--- tianshou/policy/base.py
+++ tianshou/policy/base.py
@@ -220,13 +220,12 @@ class BasePolicy(ABC, nn.Module):
         Use Implementation of Generalized Advantage Estimator (arXiv:1506.02438)
         to calculate q function/reward to go of given batch.
 
-        :param batch: a data batch which contains several episodes of data in
+        :param Batch batch: a data batch which contains several episodes of data in
             sequential order. Mind that the end of each finished episode of batch
             should be marked by done flag, unfinished (or collecting) episodes will be
             recongized by buffer.unfinished_index().
-        :type batch: :class:`~tianshou.data.Batch`
-        :param numpy.ndarray indice: tell batch's location in buffer, batch is
-            equal to buffer[indice].
+        :param numpy.ndarray indice: tell batch's location in buffer, batch is equal to
+            buffer[indice].
         :param np.ndarray v_s_: the value function of all next states :math:`V(s')`.
         :param float gamma: the discount factor, should be in [0, 1]. Default to 0.99.
         :param float gae_lambda: the parameter for Generalized Advantage Estimation,
