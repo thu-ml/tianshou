ec4ff48ce61eefb6e394f4e92fc300e7c44b7349
ChenDRAG <40993476+ChenDRAG@users.noreply.github.com>
2021-02-24 12:36:08 +0800

    Update examples/atari/atari_dqn.py
    Co-authored-by: n+e <trinkle23897@qq.com>

--- examples/atari/atari_dqn.py
+++ examples/atari/atari_dqn.py
@@ -117,7 +117,7 @@ def test_dqn(args=get_args()):
         else:
             eps = args.eps_train_final
         policy.set_eps(eps)
-        logger.write('train/eps', eps, env_step)
+        logger.write('train/eps', env_step, eps)
 
     def test_fn(epoch, env_step):
         policy.set_eps(args.eps_test)
