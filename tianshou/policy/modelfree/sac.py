c38b893b1aacdd09678fa473ca5b6bfdc463d031
chy <308604256@qq.com>
2021-03-01 18:59:00 +0800

    offpolicy update

--- tianshou/policy/modelfree/sac.py
+++ tianshou/policy/modelfree/sac.py
@@ -115,7 +115,11 @@ class SACPolicy(DDPGPolicy):
             x = dist.rsample()
         y = torch.tanh(x)
         act = y * self._action_scale + self._action_bias
+        # __eps is used to avoid log of zero/negative number.
         y = self._action_scale * (1 - y.pow(2)) + self.__eps
+        # Compute logprob from Gaussian, and then apply correction for Tanh squashing.
+        # You can check out the original SAC paper (arXiv 1801.01290): Eq 21.
+        # in appendix C to get some understanding of this equation.
         log_prob = dist.log_prob(x).unsqueeze(-1)
         log_prob = log_prob - torch.log(y).sum(-1, keepdim=True)
 
@@ -134,26 +138,11 @@ class SACPolicy(DDPGPolicy):
         return target_q
 
     def learn(self, batch: Batch, **kwargs: Any) -> Dict[str, float]:
-        weight = batch.pop("weight", 1.0)
-
-        # critic 1
-        current_q1 = self.critic1(batch.obs, batch.act).flatten()
-        target_q = batch.returns.flatten()
-        td1 = current_q1 - target_q
-        critic1_loss = (td1.pow(2) * weight).mean()
-        # critic1_loss = F.mse_loss(current_q1, target_q)
-        self.critic1_optim.zero_grad()
-        critic1_loss.backward()
-        self.critic1_optim.step()
-
-        # critic 2
-        current_q2 = self.critic2(batch.obs, batch.act).flatten()
-        td2 = current_q2 - target_q
-        critic2_loss = (td2.pow(2) * weight).mean()
-        # critic2_loss = F.mse_loss(current_q2, target_q)
-        self.critic2_optim.zero_grad()
-        critic2_loss.backward()
-        self.critic2_optim.step()
+        # critic 1&2
+        td1, critic1_loss = self._mse_optimizer(
+            batch, self.critic1, self.critic1_optim)
+        td2, critic2_loss = self._mse_optimizer(
+            batch, self.critic2, self.critic2_optim)
         batch.weight = (td1 + td2) / 2.0  # prio-buffer
 
         # actor
