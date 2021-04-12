3b3ee126030799200802632ff13d6da68c692565
Trinkle23897 <trinkle23897@gmail.com>
2021-04-12 10:54:33 +0800

    try to fix some pep8

--- examples/mujoco/plotter.py
+++ examples/mujoco/plotter.py
@@ -9,6 +9,7 @@ import argparse
 from tools import group_files
 import csv
 
+
 def smooth(y, radius, mode='two_sided', valid_only=False):
     '''
     Smooth signal y, where radius is determines the size of the window
@@ -19,20 +20,23 @@ def smooth(y, radius, mode='two_sided', valid_only=False):
     valid_only: put nan in entries where the full-sized window is not available
     '''
     assert mode in ('two_sided', 'causal')
-    if len(y) < 2*radius+1:
+    if len(y) < 2 * radius + 1:
         return np.ones_like(y) * y.mean()
     elif mode == 'two_sided':
-        convkernel = np.ones(2 * radius+1)
-        out = np.convolve(y, convkernel, mode='same') / np.convolve(np.ones_like(y), convkernel, mode='same')
+        convkernel = np.ones(2 * radius + 1)
+        out = np.convolve(y, convkernel, mode='same') / \
+            np.convolve(np.ones_like(y), convkernel, mode='same')
         if valid_only:
             out[:radius] = out[-radius:] = np.nan
     elif mode == 'causal':
         convkernel = np.ones(radius)
-        out = np.convolve(y, convkernel,mode='full') / np.convolve(np.ones_like(y), convkernel, mode='full')
-        out = out[:-radius+1]
+        out = np.convolve(y, convkernel, mode='full') / \
+            np.convolve(np.ones_like(y), convkernel, mode='full')
+        out = out[:-radius + 1]
         if valid_only:
             out[:radius] = np.nan
     return out
+
 
 COLORS = ([
     # deepmind style
@@ -43,8 +47,8 @@ COLORS = ([
     '#F0E442',
     # built-in color
     'blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'purple', 'pink',
-    'brown', 'orange', 'teal',  'lightblue', 'lime', 'lavender', 'turquoise',
-    'darkgreen', 'tan', 'salmon', 'gold',  'darkred', 'darkblue',
+    'brown', 'orange', 'teal', 'lightblue', 'lime', 'lavender', 'turquoise',
+    'darkgreen', 'tan', 'salmon', 'gold', 'darkred', 'darkblue',
     # personal color
     '#313695',  # DARK BLUE
     '#74add1',  # LIGHT BLUE
@@ -56,6 +60,7 @@ COLORS = ([
     '#ffc832',  # YELLOW
     '#000000',  # BLACK
 ])
+
 
 def csv2numpy(csv_file):
     csv_dict = {}
@@ -70,6 +75,7 @@ def csv2numpy(csv_file):
         csv_dict[k] = np.stack(v)
     return csv_dict
 
+
 def find_all_files(rootdir, pattern):
     "find all files in root dir according to relative pattern"
     file_list = []
@@ -79,6 +85,7 @@ def find_all_files(rootdir, pattern):
             if re.match(pattern, absolute_path):
                 file_list.append(absolute_path)
     return file_list
+
 
 def plot_ax(
     ax,
@@ -109,18 +116,21 @@ def plot_ax(
         ax.plot(x, y, color=color)
         if shaded_std and ykey + ':shaded' in csv_dict:
             y_shaded = smooth(csv_dict[ykey + ':shaded'], radius=smooth_radius)
-            ax.fill_between(x, y - y_shaded,    y + y_shaded, color=color, alpha=.2)
+            ax.fill_between(x, y - y_shaded, y + y_shaded, color=color, alpha=.2)
 
     ax.legend(legneds, loc=2 if legend_outside else None,
-              bbox_to_anchor=(1,1) if legend_outside else None)
+              bbox_to_anchor=(1, 1) if legend_outside else None)
     ax.xaxis.set_major_formatter(mticker.EngFormatter())
     if xlim is not None:
         ax.xlim((0, xlim))
     # add title
     ax.set_title(title)
     # add labels
-    if xlabel is not None: ax.xaxis.set_label(xlabel)
-    if ylabel is not None: ax.yaxis.set_label(ylabel)
+    if xlabel is not None:
+        ax.xaxis.set_label(xlabel)
+    if ylabel is not None:
+        ax.yaxis.set_label(ylabel)
+
 
 def plot_figure(
     file_lists,
@@ -131,15 +141,16 @@ def plot_figure(
     sharey=False,
     title=None,
     **kwargs,
-    ):
+):
     if not group_pattern:
-        fig, ax = plt.subplots(figsize=(fig_length , fig_width)) # TODO
+        fig, ax = plt.subplots(figsize=(fig_length, fig_width))  # TODO
         plot_ax(ax, file_lists, **kwargs)
     else:
         res = group_files(file_lists, group_pattern)
         row_n = int(np.ceil(len(res) / 3))
         col_n = min(len(res), 3)
-        fig, axes = plt.subplots(row_n, col_n, sharex=sharex, sharey=sharey, figsize=(fig_length * col_n, fig_width * row_n), squeeze=False)
+        fig, axes = plt.subplots(row_n, col_n, sharex=sharex, sharey=sharey, figsize=(
+            fig_length * col_n, fig_width * row_n), squeeze=False)
         axes = axes.flatten()
         for i, (k, v) in enumerate(res.items()):
             plot_ax(axes[i], v, title=k, **kwargs)
@@ -147,11 +158,12 @@ def plot_figure(
     if title:
         fig.suptitle(title, fontsize=20)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='plotter')
-    parser.add_argument('--fig_length', type=int, default=6, 
+    parser.add_argument('--fig_length', type=int, default=6,
                         help='matplotlib figure length (default: 6)')
-    parser.add_argument('--fig_width', type=int, default=6, 
+    parser.add_argument('--fig_width', type=int, default=6,
                         help='matplotlib figure width (default: 6)')
     parser.add_argument('--style', default='seaborn',
                         help='matplotlib figure style (default: seaborn)')
@@ -163,8 +175,10 @@ if __name__ == "__main__":
                         help='y-axis key in csv file (default: rew)')
     parser.add_argument('--smooth', type=int, default=0,
                         help='smooth radius of y axis (default: 0)')
-    parser.add_argument('--xlabel', default='Timesteps', help='matplotlib figure xlabel')
-    parser.add_argument('--ylabel', default='Episode Reward', help='matplotlib figure ylabel')
+    parser.add_argument('--xlabel', default='Timesteps',
+                        help='matplotlib figure xlabel')
+    parser.add_argument('--ylabel', default='Episode Reward',
+                        help='matplotlib figure ylabel')
     parser.add_argument('--shaded_std', action='store_true',
                         help='shaded region corresponding to standard deviation of the group')
     parser.add_argument('--sharex', action='store_true',
@@ -173,17 +187,22 @@ if __name__ == "__main__":
                         help='whether to share y axis within multiple sub-figures')
     parser.add_argument('--legend_outside', action='store_true',
                         help='place the legend outside of the figure')
-    parser.add_argument('--xlim', type=int, default=None, help='x-axis limitation (default: None)')
+    parser.add_argument('--xlim', type=int, default=None,
+                        help='x-axis limitation (default: None)')
     parser.add_argument('--root_dir', default='./', help='root dir (default: ./)')
-    parser.add_argument('--file_pattern', type=str, default=".*/test_rew_\d+seeds.csv$", help='regular expression to determine whether or not to include target csv_file, default to including all test_rew_{num}seeds.csv file under rootdir')
-    parser.add_argument('--group_pattern', type=str, default=r"(/|^)\w*?\-v\d(/|$)", help='regular expression to group files in sub-figure, default to grouping according to env_name dir, '' means no grouping')
+    parser.add_argument('--file_pattern', type=str, default=r".*/test_rew_\d+seeds.csv$",
+                        help='regular expression to determine whether or not to include target csv_file, default to including all test_rew_{num}seeds.csv file under rootdir')
+    parser.add_argument('--group_pattern', type=str, default=r"(/|^)\w*?\-v\d(/|$)",
+                        help='regular expression to group files in sub-figure, default to grouping according to env_name dir, '' means no grouping')
     parser.add_argument('--legend_pattern', type=str, default=".*",
                         help='regular expression to extract legend from csv file path, default to using file path as legend name.')
     parser.add_argument('--show', action='store_true', help='show figure')
-    parser.add_argument('--output_path', type=str, help='figure save path', default="./figure.png")
-    parser.add_argument('--dpi', type=int, default=400, help='figure dpi (default: 400)')
+    parser.add_argument('--output_path', type=str,
+                        help='figure save path', default="./figure.png")
+    parser.add_argument('--dpi', type=int, default=400,
+                        help='figure dpi (default: 400)')
     args = parser.parse_args()
-    # change working directory to root_dir 
+    # change working directory to root_dir
     os.chdir(args.root_dir)
     file_lists = find_all_files(args.root_dir, re.compile(args.file_pattern))
     file_lists = [os.path.relpath(f, args.root_dir) for f in file_lists]
--- examples/mujoco/tools.py
+++ examples/mujoco/tools.py
@@ -1,13 +1,11 @@
 import numpy as np
-from torch.utils.tensorboard import SummaryWriter
 from tensorboard.backend.event_processing import event_accumulator
-import json
 from contextlib import ExitStack
-import gym
 import re
 import csv
 import os
 import argparse
+
 
 def find_all_files(root_dir, pattern):
     "find all files in root dir according to relative pattern"
@@ -19,12 +17,13 @@ def find_all_files(root_dir, pattern):
                 file_list.append(absolute_path)
     return file_list
 
-def convert_tfevents_to_csv(root_dir = './', pattern=re.compile(".*tfevents[^/]*$")):
+
+def convert_tfevents_to_csv(root_dir='./', pattern=re.compile(".*tfevents[^/]*$")):
     """recursively find all tfevent file under root_dir, and create
-    a csv file to track test/rew. This function assumes that 
+    a csv file to track test/rew. This function assumes that
     there is at most one tfevents file in each directory and will add suffix
     to that directory.#TODO this need to be optimized later
-    you can use 
+    you can use
     rl_plotter --save --avg_group --shaded_std --filename=test_rew --smooth=0
     to create standard rl reward graph.
     for more detail, please refer to https://github.com/gxywy/rl-plotter
@@ -40,7 +39,7 @@ def convert_tfevents_to_csv(root_dir = './', pattern=re.compile(".*tfevents[^/]*
     sorted_files = []
     for tfevent_file in tfevent_files:
         print("handling " + tfevent_file)
-        ea=event_accumulator.EventAccumulator(tfevent_file) 
+        ea = event_accumulator.EventAccumulator(tfevent_file)
         ea.Reload()
         initial_time = ea._first_event_timestamp
         output_file = os.path.join(os.path.split(tfevent_file)[0], "test_rew.csv")
@@ -50,23 +49,34 @@ def convert_tfevents_to_csv(root_dir = './', pattern=re.compile(".*tfevents[^/]*
             writer.writeheader()
             test_rews = ea.scalars.Items('test/rew')
             for test_rew in test_rews:
-                episode_info = {'rew': round(test_rew.value, 4), 'env_step': round(test_rew.step, 4), 'time': round(test_rew.wall_time - initial_time, 4)}
+                episode_info = {
+                    'rew': round(test_rew.value, 4),
+                    'env_step': round(test_rew.step, 4),
+                    'time': round(test_rew.wall_time - initial_time, 4),
+                }
                 writer.writerow(episode_info)
     return sorted_files
 
-def concatenate_csv(root_dir = './', output_path = None, pattern=re.compile(".*/test_rew.csv$")):
-    """for all algorithm-env pair in root_dir, concatenate all test/rew under a single csv file"""
+
+def concatenate_csv(
+    root_dir='./', output_path=None, pattern=re.compile(r".*/test_rew.csv$")
+):
+    """for all algorithm-env pair in root_dir, concatenate all test/rew under a single csv file
+    """
     csv_files = find_all_files(root_dir, pattern)
     assert len(csv_files) >= 1
     # readers = [csv.DictReader(open(csv_file, newline='')) for csv_file in csv_files]
     relative_paths = [os.path.relpath(csv_file, root_dir) for csv_file in csv_files]
     with ExitStack() as stack:
         if output_path is None:
-            output_path = os.path.join(root_dir, "test_rew_{}seeds.csv".format(len(csv_files)))
+            output_path = os.path.join(
+                root_dir, "test_rew_{}seeds.csv".format(len(csv_files)))
             print("Concatenating {}.".format(output_path))
 
-        readers = [csv.DictReader(stack.enter_context(open(csv_file, newline=''))) for csv_file in csv_files]
-        fieldnames = ['env_step', 'rew', 'rew:shaded'] + ['rew:' + p for p in relative_paths]
+        readers = [csv.DictReader(stack.enter_context(
+            open(csv_file, newline=''))) for csv_file in csv_files]
+        fieldnames = ['env_step', 'rew', 'rew:shaded'] + \
+            ['rew:' + p for p in relative_paths]
         output_file = stack.enter_context(open(output_path, 'w'))
         writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -81,7 +91,9 @@ def concatenate_csv(root_dir = './', output_path = None, pattern=re.compile(".*/
             writer.writerow(episode_info)
             line_num += 1
         # summary
-        print("{} is generated, concatenating {} data lines from {} files.".format(output_path, line_num, len(csv_files)))
+        print("{} is generated, concatenating {} data lines from {} files.".format(
+            output_path, line_num, len(csv_files)))
+
 
 def group_files(file_list, pattern):
     res = {}
@@ -93,21 +105,23 @@ def group_files(file_list, pattern):
         res[key].append(f)
     return res
 
-def analysis_data(root_dir, pattern="^.*?(?=/[^/]*seed_\d.*?/)"):
+
+def analysis_data(root_dir, pattern=r"^.*?(?=/[^/]*seed_\d.*?/)"):
     sorted_files = convert_tfevents_to_csv(root_dir)
     dirs = list(group_files(sorted_files, pattern).keys())
     for dir in dirs:
         concatenate_csv(dir)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--root_dir', type=str, default=None)
-    parser.add_argument('--concatenate_pattern', type=str, default="^.*?(?=/[^/]*seed_\d.*?/)")
+    parser.add_argument('--concatenate_pattern', type=str,
+                        default=r"^.*?(?=/[^/]*seed_\d.*?/)")
     args = parser.parse_args()
     args.root_dir = "/mfs/huayu/mujoco_benchmark"
-    dirs = list(group_files(find_all_files(args.root_dir, r".*/test_rew.csv$"), args.concatenate_pattern).keys())
+    dirs = list(group_files(find_all_files(args.root_dir, r".*/test_rew.csv$"),
+                            args.concatenate_pattern).keys())
     for dir in dirs:
         concatenate_csv(dir)
     # analysis_data(args.root_dir, pattern=args.concatenate_pattern)
-    
