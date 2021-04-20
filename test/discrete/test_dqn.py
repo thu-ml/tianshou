9fe7e93ae3b9e7442212e5224a85dc63c765a382
Trinkle23897 <trinkle23897@gmail.com>
2021-04-20 16:15:15 +0800

    fix test

--- test/discrete/test_dqn.py
+++ test/discrete/test_dqn.py
@@ -136,7 +136,7 @@ def test_dqn(args=get_args()):
 
     # save buffer in pickle format, for imitation learning unittest
     buf = VectorReplayBuffer(args.buffer_size, buffer_num=len(test_envs))
-    policy.set_eps(0.3)  # result in ~190
+    policy.set_eps(0.2)
     collector = Collector(policy, test_envs, buf, exploration_noise=True)
     result = collector.collect(n_step=args.buffer_size)
     pickle.dump(buf, open(args.save_buffer_name, "wb"))
