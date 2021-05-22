14952bd343afd847727a8ab67976781ebd2265ff
Yi Su <yi.su@antgroup.com>
2021-05-23 04:49:57 +0800

    better parameter naming

--- tianshou/policy/modelfree/iqn.py
+++ tianshou/policy/modelfree/iqn.py
@@ -14,10 +14,12 @@ class IQNPolicy(QRDQNPolicy):
         :class:`~tianshou.policy.BasePolicy`. (s -> logits)
     :param torch.optim.Optimizer optim: a torch.optim for optimizing the model.
     :param float discount_factor: in [0, 1].
-    :param int K: the number of samples for policy evaluation. Default to 32.
-    :param int N: the number of samples for online model in training. Default to 8.
-    :param int N_prime: the number of samples for target model in training.
-        Default to 8.
+    :param int sample_size: the number of samples for policy evaluation.
+        Default to 32.
+    :param int online_sample_size: the number of samples for online model
+        in training. Default to 8.
+    :param int target_sample_size: the number of samples for target model
+        in training. Default to 8.
     :param int estimation_step: the number of steps to look ahead. Default to 1.
     :param int target_update_freq: the target network update frequency (0 if
         you do not use the target network).
@@ -35,36 +37,37 @@ class IQNPolicy(QRDQNPolicy):
         model: torch.nn.Module,
         optim: torch.optim.Optimizer,
         discount_factor: float = 0.99,
-        K: int = 32,
-        N: int = 8,
-        N_prime: int = 8,
+        sample_size: int = 32,
+        online_sample_size: int = 8,
+        target_sample_size: int = 8,
         estimation_step: int = 1,
         target_update_freq: int = 0,
         reward_normalization: bool = False,
         **kwargs: Any,
     ) -> None:
-        super().__init__(model, optim, discount_factor, K, estimation_step,
+        super().__init__(model, optim, discount_factor, sample_size, estimation_step,
                          target_update_freq, reward_normalization, **kwargs)
-        assert K > 1, "K should be greater than 1"
-        assert N > 1, "N should be greater than 1"
-        assert N_prime > 1, "N_prime should be greater than 1"
-        self._K = K  # for policy eval
-        self._N = N  # for online model
-        self._N_prime = N_prime  # for target model
+        assert sample_size > 1, "sample_size should be greater than 1"
+        assert online_sample_size > 1, "online_sample_size should be greater than 1"
+        assert target_sample_size > 1, "target_sample_size should be greater than 1"
+        self._sample_size = sample_size  # for policy eval
+        self._online_sample_size = online_sample_size
+        self._target_sample_size = target_sample_size
         # set sample size for online and target model
-        self.model.sample_size = self._N
+        self.model.sample_size = self._online_sample_size  # type: ignore
         if self._target:
-            self.model_old.sample_size = self._N_prime
+            self.model_old.sample_size = self._target_sample_size  # type: ignore
 
     def train(self, mode: bool = True) -> "IQNPolicy":
-        self.model.sample_size = self._N if mode else self._K
         super().train(mode)
+        self.model.sample_size = (self._online_sample_size if mode  # type: ignore
+                                  else self._sample_size)  # type: ignore
         return self
 
     def sync_weight(self) -> None:
         """Synchronize the weight for the target network."""
         self.model_old.load_state_dict(self.model.state_dict())  # type: ignore
-        self.model_old.sample_size = self._N_prime
+        self.model_old.sample_size = self._target_sample_size  # type: ignore
 
     def learn(self, batch: Batch, **kwargs: Any) -> Dict[str, float]:
         if self._target and self._iter % self._freq == 0:
