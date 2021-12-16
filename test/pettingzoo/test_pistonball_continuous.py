4b8e057a5a3ec9281253ca7f3061b4215f1c5ceb
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-12-16 15:21:35 +0900

    add pistonball continuous on policy test

---
+++ test/pettingzoo/test_pistonball_continuous.py
@@ -0,0 +1,21 @@
+import pprint
+
+from pistonball_continuous import get_args, train_agent, watch
+
+
+def test_piston_ball_continuous(args=get_args()):
+    if args.watch:
+        watch(args)
+        return
+
+    result, agent = train_agent(args)
+    # assert result["best_reward"] >= args.win_rate
+
+    if __name__ == '__main__':
+        pprint.pprint(result)
+        # Let's watch its performance!
+        watch(args, agent)
+
+
+if __name__ == '__main__':
+    test_piston_ball_continuous(get_args())
