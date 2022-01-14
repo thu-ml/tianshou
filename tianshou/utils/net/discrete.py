34f7a2883cd471bc17d43098d81b244749dd0fc9
Yi Su <yi.su@antgroup.com>
2022-01-14 10:03:24 +0800

    minor change

--- tianshou/utils/net/discrete.py
+++ tianshou/utils/net/discrete.py
@@ -434,6 +434,6 @@ class IntrinsicCuriosityModule(nn.Module):
         phi2_hat = self.forward_model(
             torch.cat([phi1, F.one_hot(act, num_classes=self.action_dim)], dim=1)
         )
-        mse_loss = 0.5 * ((phi2_hat - phi2)**2).sum(1)
+        mse_loss = 0.5 * F.mse_loss(phi2_hat, phi2, reduction="none").sum(1)
         act_hat = self.inverse_model(torch.cat([phi1, phi2], dim=1))
         return mse_loss, act_hat
