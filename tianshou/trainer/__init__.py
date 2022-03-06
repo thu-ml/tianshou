88cb63c35ee8da7ee0fdb9f88f0f18c2233ab88a
R107333 <ja.martin.h@repsol.com>
2022-03-06 20:16:21 +0100

    Expose new _iter versions and Iterator Classes

--- tianshou/trainer/__init__.py
+++ tianshou/trainer/__init__.py
@@ -4,16 +4,20 @@
 
 from tianshou.trainer.utils import test_episode, gather_info
 from tianshou.trainer.onpolicy import onpolicy_trainer, onpolicy_trainer_generator
-from tianshou.trainer.offpolicy import offpolicy_trainer, offpolicy_trainer_generator
-from tianshou.trainer.offline import offline_trainer, offline_trainer_iter
+from tianshou.trainer.offpolicy import offpolicy_trainer, offpolicy_trainer_iter,\
+    OffPolicyTrainer
+from tianshou.trainer.offline import offline_trainer, offline_trainer_iter,\
+    OffLineTrainer
 
 __all__ = [
     "offpolicy_trainer",
-    "offpolicy_trainer_generator",
+    "offpolicy_trainer_iter",
+    "OffPolicyTrainer",
     "onpolicy_trainer",
     "onpolicy_trainer_generator",
     "offline_trainer",
     "offline_trainer_iter",
+    "OffLineTrainer",
     "test_episode",
     "gather_info",
 ]
