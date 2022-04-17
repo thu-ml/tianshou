10bc5ca146d074a1f261b09719ba479219968cdf
Alex Nikulkov <alexnik@fb.com>
2022-04-16 20:00:06 -0700

    fix docstrings

--- tianshou/utils/lr_scheduler.py
+++ tianshou/utils/lr_scheduler.py
@@ -4,17 +4,32 @@ import torch
 
 
 class MultipleLRSchedulers:
+    """A wrapper for multiple learning rate schedulers.
+
+    Every time :meth:`~tianshou.utils.MultipleLRSchedulers.step()` is called, \
+    it calls the step() method of each of the schedulers that it contains.
+    """
 
     def __init__(self, *args: torch.optim.lr_scheduler.LambdaLR):
         self.schedulers = args
 
     def step(self) -> None:
+        """Take a step in each of the learning rate schedulers."""
         for scheduler in self.schedulers:
             scheduler.step()
 
     def state_dict(self) -> List[Dict]:
+        """Get state dictionaries for each of the learning rate schedulers.
+
+        :return: A list of state_dicts of learning rate schedulers
+        """
         return [s.state_dict() for s in self.schedulers]
 
     def load_state_dict(self, state_dict: List[Dict]) -> None:
+        """Load states from  dictionaries.
+
+        :param List[Dict] state_dict: A list of learning rate scheduler
+            state_dicts, in the same order as the schedulers.
+        """
         for (s, sd) in zip(self.schedulers, state_dict):
             s.__dict__.update(sd)
