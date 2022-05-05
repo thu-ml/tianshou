2a7c1517382177f25095753c9b01b72819dc3ac4
Jiayi Weng <trinkle23897@gmail.com>
2022-05-05 07:55:15 -0400

    Add vecenv wrappers for obs_norm to support running mujoco experiment with envpool (#628)
    - add VectorEnvWrapper and VectorEnvNormObs
    - obs_rms store in policy save/load
    - align mujoco scripts with atari: obs_norm, envpool, wandb and README

--- examples/mujoco/mujoco_sac.py
+++ examples/mujoco/mujoco_sac.py
@@ -5,81 +5,77 @@ import datetime
 import os
 import pprint
 
-import gym
 import numpy as np
 import torch
+from mujoco_env import make_mujoco_env
 from torch.utils.tensorboard import SummaryWriter
 
 from tianshou.data import Collector, ReplayBuffer, VectorReplayBuffer
-from tianshou.env import SubprocVectorEnv
 from tianshou.policy import SACPolicy
 from tianshou.trainer import offpolicy_trainer
-from tianshou.utils import TensorboardLogger
+from tianshou.utils import TensorboardLogger, WandbLogger
 from tianshou.utils.net.common import Net
 from tianshou.utils.net.continuous import ActorProb, Critic
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, default='Ant-v3')
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--buffer-size', type=int, default=1000000)
-    parser.add_argument('--hidden-sizes', type=int, nargs='*', default=[256, 256])
-    parser.add_argument('--actor-lr', type=float, default=1e-3)
-    parser.add_argument('--critic-lr', type=float, default=1e-3)
-    parser.add_argument('--gamma', type=float, default=0.99)
-    parser.add_argument('--tau', type=float, default=0.005)
-    parser.add_argument('--alpha', type=float, default=0.2)
-    parser.add_argument('--auto-alpha', default=False, action='store_true')
-    parser.add_argument('--alpha-lr', type=float, default=3e-4)
+    parser.add_argument("--task", type=str, default="Ant-v3")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--buffer-size", type=int, default=1000000)
+    parser.add_argument("--hidden-sizes", type=int, nargs="*", default=[256, 256])
+    parser.add_argument("--actor-lr", type=float, default=1e-3)
+    parser.add_argument("--critic-lr", type=float, default=1e-3)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--alpha", type=float, default=0.2)
+    parser.add_argument("--auto-alpha", default=False, action="store_true")
+    parser.add_argument("--alpha-lr", type=float, default=3e-4)
     parser.add_argument("--start-timesteps", type=int, default=10000)
-    parser.add_argument('--epoch', type=int, default=200)
-    parser.add_argument('--step-per-epoch', type=int, default=5000)
-    parser.add_argument('--step-per-collect', type=int, default=1)
-    parser.add_argument('--update-per-step', type=int, default=1)
-    parser.add_argument('--n-step', type=int, default=1)
-    parser.add_argument('--batch-size', type=int, default=256)
-    parser.add_argument('--training-num', type=int, default=1)
-    parser.add_argument('--test-num', type=int, default=10)
-    parser.add_argument('--logdir', type=str, default='log')
-    parser.add_argument('--render', type=float, default=0.)
+    parser.add_argument("--epoch", type=int, default=200)
+    parser.add_argument("--step-per-epoch", type=int, default=5000)
+    parser.add_argument("--step-per-collect", type=int, default=1)
+    parser.add_argument("--update-per-step", type=int, default=1)
+    parser.add_argument("--n-step", type=int, default=1)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--training-num", type=int, default=1)
+    parser.add_argument("--test-num", type=int, default=10)
+    parser.add_argument("--logdir", type=str, default="log")
+    parser.add_argument("--render", type=float, default=0.)
     parser.add_argument(
-        '--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu'
+        "--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu"
     )
-    parser.add_argument('--resume-path', type=str, default=None)
+    parser.add_argument("--resume-path", type=str, default=None)
+    parser.add_argument("--resume-id", type=str, default=None)
     parser.add_argument(
-        '--watch',
+        "--logger",
+        type=str,
+        default="tensorboard",
+        choices=["tensorboard", "wandb"],
+    )
+    parser.add_argument("--wandb-project", type=str, default="mujoco.benchmark")
+    parser.add_argument(
+        "--watch",
         default=False,
-        action='store_true',
-        help='watch the play of pre-trained policy only'
+        action="store_true",
+        help="watch the play of pre-trained policy only",
     )
     return parser.parse_args()
 
 
 def test_sac(args=get_args()):
-    env = gym.make(args.task)
+    env, train_envs, test_envs = make_mujoco_env(
+        args.task, args.seed, args.training_num, args.test_num, obs_norm=False
+    )
     args.state_shape = env.observation_space.shape or env.observation_space.n
     args.action_shape = env.action_space.shape or env.action_space.n
     args.max_action = env.action_space.high[0]
     print("Observations shape:", args.state_shape)
     print("Actions shape:", args.action_shape)
     print("Action range:", np.min(env.action_space.low), np.max(env.action_space.high))
-    # train_envs = gym.make(args.task)
-    if args.training_num > 1:
-        train_envs = SubprocVectorEnv(
-            [lambda: gym.make(args.task) for _ in range(args.training_num)]
-        )
-    else:
-        train_envs = gym.make(args.task)
-    # test_envs = gym.make(args.task)
-    test_envs = SubprocVectorEnv(
-        [lambda: gym.make(args.task) for _ in range(args.test_num)]
-    )
     # seed
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    train_envs.seed(args.seed)
-    test_envs.seed(args.seed)
     # model
     net_a = Net(args.state_shape, hidden_sizes=args.hidden_sizes, device=args.device)
     actor = ActorProb(
@@ -88,7 +84,7 @@ def test_sac(args=get_args()):
         max_action=args.max_action,
         device=args.device,
         unbounded=True,
-        conditioned_sigma=True
+        conditioned_sigma=True,
     ).to(args.device)
     actor_optim = torch.optim.Adam(actor.parameters(), lr=args.actor_lr)
     net_c1 = Net(
@@ -96,14 +92,14 @@ def test_sac(args=get_args()):
         args.action_shape,
         hidden_sizes=args.hidden_sizes,
         concat=True,
-        device=args.device
+        device=args.device,
     )
     net_c2 = Net(
         args.state_shape,
         args.action_shape,
         hidden_sizes=args.hidden_sizes,
         concat=True,
-        device=args.device
+        device=args.device,
     )
     critic1 = Critic(net_c1, device=args.device).to(args.device)
     critic1_optim = torch.optim.Adam(critic1.parameters(), lr=args.critic_lr)
@@ -127,7 +123,7 @@ def test_sac(args=get_args()):
         gamma=args.gamma,
         alpha=args.alpha,
         estimation_step=args.n_step,
-        action_space=env.action_space
+        action_space=env.action_space,
     )
 
     # load a previous policy
@@ -143,16 +139,31 @@ def test_sac(args=get_args()):
     train_collector = Collector(policy, train_envs, buffer, exploration_noise=True)
     test_collector = Collector(policy, test_envs)
     train_collector.collect(n_step=args.start_timesteps, random=True)
+
     # log
-    t0 = datetime.datetime.now().strftime("%m%d_%H%M%S")
-    log_file = f'seed_{args.seed}_{t0}-{args.task.replace("-", "_")}_sac'
-    log_path = os.path.join(args.logdir, args.task, 'sac', log_file)
+    now = datetime.datetime.now().strftime("%y%m%d-%H%M%S")
+    args.algo_name = "sac"
+    log_name = os.path.join(args.task, args.algo_name, str(args.seed), now)
+    log_path = os.path.join(args.logdir, log_name)
+
+    # logger
+    if args.logger == "wandb":
+        logger = WandbLogger(
+            save_interval=1,
+            name=log_name.replace(os.path.sep, "__"),
+            run_id=args.resume_id,
+            config=args,
+            project=args.wandb_project,
+        )
     writer = SummaryWriter(log_path)
     writer.add_text("args", str(args))
-    logger = TensorboardLogger(writer)
+    if args.logger == "tensorboard":
+        logger = TensorboardLogger(writer)
+    else:  # wandb
+        logger.load(writer)
 
     def save_best_fn(policy):
-        torch.save(policy.state_dict(), os.path.join(log_path, 'policy.pth'))
+        torch.save(policy.state_dict(), os.path.join(log_path, "policy.pth"))
 
     if not args.watch:
         # trainer
@@ -168,7 +179,7 @@ def test_sac(args=get_args()):
             save_best_fn=save_best_fn,
             logger=logger,
             update_per_step=args.update_per_step,
-            test_in_train=False
+            test_in_train=False,
         )
         pprint.pprint(result)
 
@@ -180,5 +191,5 @@ def test_sac(args=get_args()):
     print(f'Final reward: {result["rews"].mean()}, length: {result["lens"].mean()}')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_sac()
---
+++ tianshou/env/venv_wrappers.py
@@ -0,0 +1,120 @@
+from typing import Any, List, Optional, Tuple, Union
+
+import numpy as np
+
+from tianshou.env.venvs import GYM_RESERVED_KEYS, BaseVectorEnv
+from tianshou.utils import RunningMeanStd
+
+
+class VectorEnvWrapper(BaseVectorEnv):
+    """Base class for vectorized environments wrapper."""
+
+    def __init__(self, venv: BaseVectorEnv) -> None:
+        self.venv = venv
+        self.is_async = venv.is_async
+
+    def __len__(self) -> int:
+        return len(self.venv)
+
+    def __getattribute__(self, key: str) -> Any:
+        if key in GYM_RESERVED_KEYS:  # reserved keys in gym.Env
+            return getattr(self.venv, key)
+        else:
+            return super().__getattribute__(key)
+
+    def get_env_attr(
+        self,
+        key: str,
+        id: Optional[Union[int, List[int], np.ndarray]] = None,
+    ) -> List[Any]:
+        return self.venv.get_env_attr(key, id)
+
+    def set_env_attr(
+        self,
+        key: str,
+        value: Any,
+        id: Optional[Union[int, List[int], np.ndarray]] = None,
+    ) -> None:
+        return self.venv.set_env_attr(key, value, id)
+
+    # TODO: compatible issue with reset -> (obs, info)
+    def reset(
+        self, id: Optional[Union[int, List[int], np.ndarray]] = None
+    ) -> np.ndarray:
+        return self.venv.reset(id)
+
+    def step(
+        self,
+        action: np.ndarray,
+        id: Optional[Union[int, List[int], np.ndarray]] = None,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        return self.venv.step(action, id)
+
+    def seed(
+        self,
+        seed: Optional[Union[int, List[int]]] = None,
+    ) -> List[Optional[List[int]]]:
+        return self.venv.seed(seed)
+
+    def render(self, **kwargs: Any) -> List[Any]:
+        return self.venv.render(**kwargs)
+
+    def close(self) -> None:
+        self.venv.close()
+
+
+class VectorEnvNormObs(VectorEnvWrapper):
+    """An observation normalization wrapper for vectorized environments.
+
+    :param bool update_obs_rms: whether to update obs_rms. Default to True.
+    :param float clip_obs: the maximum absolute value for observation. Default to
+        10.0.
+    :param float epsilon: To avoid division by zero.
+    """
+
+    def __init__(
+        self,
+        venv: BaseVectorEnv,
+        update_obs_rms: bool = True,
+        clip_obs: float = 10.0,
+        epsilon: float = np.finfo(np.float32).eps.item(),
+    ) -> None:
+        super().__init__(venv)
+        # initialize observation running mean/std
+        self.update_obs_rms = update_obs_rms
+        self.obs_rms = RunningMeanStd()
+        self.clip_max = clip_obs
+        self.eps = epsilon
+
+    # TODO: compatible issue with reset -> (obs, info)
+    def reset(
+        self, id: Optional[Union[int, List[int], np.ndarray]] = None
+    ) -> np.ndarray:
+        obs = self.venv.reset(id)
+        if self.obs_rms and self.update_obs_rms:
+            self.obs_rms.update(obs)
+        return self._norm_obs(obs)
+
+    def step(
+        self,
+        action: np.ndarray,
+        id: Optional[Union[int, List[int], np.ndarray]] = None,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        obs, rew, done, info = self.venv.step(action, id)
+        if self.obs_rms and self.update_obs_rms:
+            self.obs_rms.update(obs)
+        return self._norm_obs(obs), rew, done, info
+
+    def _norm_obs(self, obs: np.ndarray) -> np.ndarray:
+        if self.obs_rms:
+            obs = (obs - self.obs_rms.mean) / np.sqrt(self.obs_rms.var + self.eps)
+            obs = np.clip(obs, -self.clip_max, self.clip_max)
+        return obs
+
+    def set_obs_rms(self, obs_rms: RunningMeanStd) -> None:
+        """Set with given observation running mean/std."""
+        self.obs_rms = obs_rms
+
+    def get_obs_rms(self) -> RunningMeanStd:
+        """Return observation running mean/std."""
+        return self.obs_rms
