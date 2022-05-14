0bc2d20678a7b584c9877d301c5b2d2f7a3fbd73
BELFADIL BELFADIL <R901105@z0euw1lptmx356.rg.repsol.com>
2022-05-14 18:23:18 +0200

    Fix lint two spaces comment error

--- tianshou/env/gym_wrappers.py
+++ tianshou/env/gym_wrappers.py
@@ -15,13 +15,13 @@ class ContinuousToDiscrete(gym.ActionWrapper):
     def __init__(self, env: gym.Env, action_per_branch: int) -> None:
         super().__init__(env)
         self.action_per_branch = action_per_branch
-        low = env.action_space.low # type: ignore
-        high = env.action_space.high # type: ignore
-        num_branches = env.action_space.shape[0] # type: ignore
+        low = env.action_space.low  # type: ignore
+        high = env.action_space.high  # type: ignore
+        num_branches = env.action_space.shape[0]  # type: ignore
         self.action_space = gym.spaces.MultiDiscrete(
             [action_per_branch] * num_branches
         )
-        self.action_space.n = num_branches # type: ignore
+        self.action_space.n = num_branches  # type: ignore
         mesh = []
         for lo, hi in zip(low, high):
             mesh.append(np.linspace(lo, hi, action_per_branch))
