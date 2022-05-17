4f370f0e9ee7f98002f0668f6e2e51ce4ec88804
Michal Gregor <michal@gregor.sk>
2022-05-17 08:33:19 +0200

    Added support for disabling training progress bar. * A dummy_tqdm class added to utils: it replicates the interface used   by  trainers, but does not show the progress bar; * Added a show_progress argument to the base trainer: when   show_progress == True,  dummy_tqdm is used in place of tqdm;

--- tianshou/utils/__init__.py
+++ tianshou/utils/__init__.py
@@ -1,6 +1,7 @@
 """Utils package."""
 
 from tianshou.utils.config import tqdm_config
+from tianshou.utils.dummy_tqdm import dummy_tqdm
 from tianshou.utils.logger.base import BaseLogger, LazyLogger
 from tianshou.utils.logger.tensorboard import BasicLogger, TensorboardLogger
 from tianshou.utils.logger.wandb import WandbLogger
---
+++ tianshou/utils/dummy_tqdm.py
@@ -0,0 +1,23 @@
+class dummy_tqdm:
+    """A dummy tqdm class that keeps the total and n stats, but shows
+    no progress bar.
+
+    It supports __enter__ and __exit__, update and a dummy set_postfix:
+    the interface that trainers use.
+    """
+
+    def __init__(self, total, **kwargs):
+        self.total = total
+        self.n = 0
+
+    def set_postfix(self, **kwargs):
+        pass
+
+    def update(self, n=1):
+        self.n += n
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
