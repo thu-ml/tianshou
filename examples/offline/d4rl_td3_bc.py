5aa480d674e7be92cbfc62a08569999fe790c8f9
Yi Su <yi.su@antgroup.com>
2022-06-04 14:54:36 -0700

    fix a typo

--- examples/offline/d4rl_td3_bc.py
+++ examples/offline/d4rl_td3_bc.py
@@ -10,7 +10,7 @@ import numpy as np
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-from examples.offline.utils import load_buffer_d4rl, normalize_obs
+from examples.offline.utils import load_buffer_d4rl, normalize_all_obs_in_replay_buffer
 from tianshou.data import Collector
 from tianshou.env import SubprocVectorEnv, VectorEnvNormObs
 from tianshou.exploration import GaussianNoise
