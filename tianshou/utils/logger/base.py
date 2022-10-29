9082db779eacade5ecd27e76fc76df9f312a82cb
Yi Su <nuance1979@hotmail.com>
2022-10-29 09:36:09 -0700

    make linter happy

--- tianshou/utils/logger/base.py
+++ tianshou/utils/logger/base.py
@@ -89,6 +89,7 @@ class BaseLogger(ABC):
             self.write("update/gradient_step", step, log_data)
             self.last_log_update_step = step
 
+    @abstractmethod
     def save_data(
         self,
         epoch: int,
@@ -106,6 +107,7 @@ class BaseLogger(ABC):
         """
         pass
 
+    @abstractmethod
     def restore_data(self) -> Tuple[int, int, int]:
         """Return the metadata from existing log.
 
@@ -125,4 +127,16 @@ class LazyLogger(BaseLogger):
 
     def write(self, step_type: str, step: int, data: LOG_DATA_TYPE) -> None:
         """The LazyLogger writes nothing."""
+        pass
+
+    def save_data(
+        self,
+        epoch: int,
+        env_step: int,
+        gradient_step: int,
+        save_checkpoint_fn: Optional[Callable[[int, int, int], str]] = None,
+    ) -> None:
+        pass
+
+    def restore_data(self) -> Tuple[int, int, int]:
         pass
