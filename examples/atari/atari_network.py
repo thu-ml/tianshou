a7d51e7162dcd394bd8b545669d44418376c818b
Yi Su <yi_su@apple.com>
2022-11-13 10:37:26 -0800

    add type hints

--- examples/atari/atari_network.py
+++ examples/atari/atari_network.py
@@ -7,7 +7,9 @@ from torch import nn
 from tianshou.utils.net.discrete import NoisyLinear
 
 
-def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+def layer_init(
+    layer: nn.Module, std: float = np.sqrt(2), bias_const: float = 0.0
+) -> nn.Module:
     torch.nn.init.orthogonal_(layer.weight, std)
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
