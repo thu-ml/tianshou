a54aade730482be6b90a1f853f79bea76e420bad
Michael Panchenko <m.panchenko@appliedai.de>
2023-07-26 20:24:33 +0200

    Addition of dataclasses based config for scripts, major refactoring
    So far only for one script (mujoco_ppo_cfg), extension will follow
    
    Conflicts:
    	examples/mujoco/mujoco_env.py
    	examples/mujoco/mujoco_ppo.py
    	setup.py

--- examples/mujoco/mujoco_env.py
+++ examples/mujoco/mujoco_env.py
@@ -10,7 +10,9 @@ except ImportError:
     envpool = None
 
 
-def make_mujoco_env(task, seed, training_num, test_num, obs_norm):
+def make_mujoco_env(
+    task: str, seed: int, num_train_envs: int, num_test_envs: int, obs_norm: bool
+):
     """Wrapper function for Mujoco env.
 
     If EnvPool is installed, it will automatically switch to EnvPool's Mujoco env.
@@ -18,17 +20,16 @@ def make_mujoco_env(task, seed, training_num, test_num, obs_norm):
     :return: a tuple of (single env, training envs, test envs).
     """
     if envpool is not None:
-        train_envs = env = envpool.make_gymnasium(task, num_envs=training_num, seed=seed)
-        test_envs = envpool.make_gymnasium(task, num_envs=test_num, seed=seed)
+        train_envs = env = envpool.make_gymnasium(task, num_envs=num_train_envs, seed=seed)
+        test_envs = envpool.make_gymnasium(task, num_envs=num_test_envs, seed=seed)
     else:
         warnings.warn(
             "Recommend using envpool (pip install envpool) "
             "to run Mujoco environments more efficiently.",
         )
         env = gym.make(task)
-        train_envs = ShmemVectorEnv([lambda: gym.make(task) for _ in range(training_num)])
-        test_envs = ShmemVectorEnv([lambda: gym.make(task) for _ in range(test_num)])
-        env.seed(seed)
+        train_envs = ShmemVectorEnv([lambda: gym.make(task) for _ in range(num_train_envs)])
+        test_envs = ShmemVectorEnv([lambda: gym.make(task) for _ in range(num_test_envs)])
         train_envs.seed(seed)
         test_envs.seed(seed)
     if obs_norm:
