47b08f227db00c4bdb63471c6b8f7e4a1b1987ba
Jiayi Weng <trinkle23897@gmail.com>
2023-10-16 16:51:33 -0700

    Update tianshou/policy/multiagent/mapolicy.py

--- tianshou/policy/multiagent/mapolicy.py
+++ tianshou/policy/multiagent/mapolicy.py
@@ -233,7 +233,7 @@ class MultiAgentPolicyManager(BasePolicy):
     
     # Bug Fix. Need a train method that set all sub-policies to train mode.
     # No need for a similar eval function, as eval internally uses the train function. 
-    def train(self, mode: bool = True):
+    def train(self, mode: bool = True) -> "MultiAgentPolicyManager":
         """Set each internal policy in training mode."""
         for agent_id, policy in self.policies.items():
             policy.train(mode)
