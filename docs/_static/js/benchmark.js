d64f5b95eccfc3c86e4f053c5d271f22312de019
Michael Panchenko <m.panchenko@appliedai.de>
2024-02-09 19:41:10 +0100

    Load jQuery dynamically

--- docs/_static/js/benchmark.js
+++ docs/_static/js/benchmark.js
@@ -1,3 +1,17 @@
+// Immediately-invoked function expression - load JQuery
+(function() {
+  // Load the script
+  const script = document.createElement("script");
+  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+  script.type = 'text/javascript';
+  script.addEventListener('load', () => {
+    console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
+    // use jQuery below
+  });
+  document.head.appendChild(script);
+})();
+
+
 var mujoco_envs = [
     "Ant-v3",
     "HalfCheetah-v3",
