d3fe87b70deefc549cccec3455faf825033adfd2
Michael Panchenko <35432522+MischaPanch@users.noreply.github.com>
2024-02-12 17:38:25 +0100

    Docs: added symlinks for paths resolution, removed jquery loading (#1050)

--- docs/_static/js/copybutton.js
+++ docs/_static/js/copybutton.js
@@ -1,17 +1,3 @@
-// Immediately-invoked function expression - load JQuery
-(function() {
-  // Load the script
-  const script = document.createElement("script");
-  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
-  script.type = 'text/javascript';
-  script.addEventListener('load', () => {
-    console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
-    // use jQuery below
-  });
-  document.head.appendChild(script);
-})();
-
-
 document.addEventListener('DOMContentLoaded', function()  {
     /* Add a [>>>] button on the top-right corner of code samples to hide
      * the >>> and ... prompts and the output and thus make the code
