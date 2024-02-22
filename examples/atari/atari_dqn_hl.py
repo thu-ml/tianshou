thu-ml/tianshou

commit eb0215cf76f1fcb9ac748e2c2301419be7bbd8c5

Refactoring/mypy issues test (#1017)
Improves typing in examples and tests, towards mypy passing there.

Introduces the SpaceInfo utility

examples/atari/atari_dqn_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 
 from examples.atari.atari_network import (
@@ -48,7 +49,7 @@ def main(
     icm_lr_scale: float = 0.0,
     icm_reward_scale: float = 0.01,
     icm_forward_loss_weight: float = 0.2,
-):
+) -> None:
     log_name = os.path.join(task, "dqn", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -102,4 +103,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

examples/atari/atari_iqn_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 from collections.abc import Sequence
 
@@ -47,7 +48,7 @@ def main(
     test_num: int = 10,
     frames_stack: int = 4,
     save_buffer_name: str | None = None,  # TODO support?
-):
+) -> None:
     log_name = os.path.join(task, "iqn", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -94,4 +95,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

examples/atari/atari_ppo_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 from collections.abc import Sequence
 
@@ -53,7 +54,7 @@ def main(
     icm_lr_scale: float = 0.0,
     icm_reward_scale: float = 0.01,
     icm_forward_loss_weight: float = 0.2,
-):
+) -> None:
     log_name = os.path.join(task, "ppo", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -113,4 +114,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

examples/atari/atari_sac_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 
 from examples.atari.atari_network import (
@@ -47,7 +48,7 @@ def main(
     icm_lr_scale: float = 0.0,
     icm_reward_scale: float = 0.01,
     icm_forward_loss_weight: float = 0.2,
-):
+) -> None:
     log_name = os.path.join(task, "sac", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -100,4 +101,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

examples/mujoco/mujoco_a2c_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -40,7 +41,7 @@ def main(
     bound_action_method: Literal["clip", "tanh"] = "clip",
     lr_decay: bool = True,
     max_grad_norm: float = 0.5,
-):
+) -> None:
     log_name = os.path.join(task, "a2c", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -82,4 +83,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

examples/mujoco/mujoco_ddpg_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 from collections.abc import Sequence
 
@@ -34,7 +35,7 @@ def main(
     batch_size: int = 256,
     training_num: int = 1,
     test_num: int = 10,
-):
+) -> None:
     log_name = os.path.join(task, "ddpg", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -73,4 +74,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

examples/mujoco/mujoco_npg_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -42,7 +43,7 @@ def main(
     norm_adv: bool = True,
     optim_critic_iters: int = 20,
     actor_step_size: float = 0.1,
-):
+) -> None:
     log_name = os.path.join(task, "npg", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -84,4 +85,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

examples/mujoco/mujoco_ppo_hl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -47,7 +48,7 @@ def main(
     value_clip: bool = False,
     norm_adv: bool = False,
     recompute_adv: bool = True,
-):
+) -> None:
     log_name = os.path.join(task, "ppo", str(experiment_config.seed), datetime_tag())
 
     sampling_config = SamplingConfig(
@@ -94,4 +95,5 @@ def main(
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
+    logging.run_cli(run_with_default_config)

commit b3e72b316147ffdd2798f2c726a28938f4f2413f

Fix high-level examples (were broken by commit eb0215cf)

examples/atari/atari_dqn_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 
 from examples.atari.atari_network import (
@@ -103,5 +102,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/atari/atari_iqn_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 
@@ -95,5 +94,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/atari/atari_ppo_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 
@@ -114,5 +113,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/atari/atari_sac_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 
 from examples.atari.atari_network import (
@@ -101,5 +100,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_a2c_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -83,5 +82,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_ddpg_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 
@@ -74,5 +73,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_npg_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -85,5 +84,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_ppo_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -95,5 +94,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_redq_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -83,5 +82,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_reinforce_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -72,5 +71,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_sac_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 
@@ -80,5 +79,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_td3_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 
@@ -85,5 +84,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)

examples/mujoco/mujoco_trpo_hl.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import functools
 import os
 from collections.abc import Sequence
 from typing import Literal
@@ -89,5 +88,4 @@ def main(
 
 
 if __name__ == "__main__":
-    run_with_default_config = functools.partial(main, experiment_config=ExperimentConfig())
-    logging.run_cli(run_with_default_config)
+    logging.run_cli(main)