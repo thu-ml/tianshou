d6cd08368202d38f1dd5512b9eb23aac6343e840
Michael Panchenko <m.panchenko@appliedai.de>
2024-03-22 15:58:06 +0100

    Minor fixes in FiniteVectorEnv (created in tests)

--- test/base/test_env_finite.py
+++ test/base/test_env_finite.py
@@ -90,20 +90,20 @@ class FiniteVectorEnv(BaseVectorEnv):
 
     # END
 
-    def reset(self, id=None):
-        id = self._wrap_id(id)
+    def reset(self, env_ids=None):
+        env_ids = self._wrap_id(env_ids)
         self._reset_alive_envs()
 
         # ask super to reset alive envs and remap to current index
-        request_id = list(filter(lambda i: i in self._alive_env_ids, id))
-        obs = [None] * len(id)
-        infos = [None] * len(id)
-        id2idx = {i: k for k, i in enumerate(id)}
+        request_id = list(filter(lambda i: i in self._alive_env_ids, env_ids))
+        obs = [None] * len(env_ids)
+        infos = [None] * len(env_ids)
+        id2idx = {i: k for k, i in enumerate(env_ids)}
         if request_id:
             for k, o, info in zip(request_id, *super().reset(request_id), strict=True):
                 obs[id2idx[k]] = o
                 infos[id2idx[k]] = info
-        for i, o in zip(id, obs, strict=True):
+        for i, o in zip(env_ids, obs, strict=True):
             if o is None and i in self._alive_env_ids:
                 self._alive_env_ids.remove(i)
 
@@ -121,7 +121,7 @@ class FiniteVectorEnv(BaseVectorEnv):
             self.reset()
             raise StopIteration
 
-        return np.stack(obs), infos
+        return np.stack(obs), np.array(infos)
 
     def step(self, action, id=None):
         id = self._wrap_id(id)
@@ -209,6 +209,7 @@ def test_finite_dummy_vector_env() -> None:
     for _ in range(3):
         envs.tracker = MetricTracker()
         try:
+            # TODO: why on earth 10**18?
             test_collector.collect(n_step=10**18)
         except StopIteration:
             envs.tracker.validate()
