e327caae7edbf24a3dcd238182a2d1b3875c5f09
Michael Panchenko <m.panchenko@appliedai.de>
2024-04-02 14:25:21 +0200

    Don't raise error on len of empty Batch

--- tianshou/data/batch.py
+++ tianshou/data/batch.py
@@ -870,16 +870,17 @@ class Batch(BatchProtocol):
         """Return len(self)."""
         lens = []
         for obj in self.__dict__.values():
+            # TODO: causes inconsistent behavior to batch with empty batches
+            #  and batch with empty sequences of other type. Remove, but only after
+            #  Buffer and Collectors have been improved to no longer rely on this
             if isinstance(obj, Batch) and obj.is_empty(recurse=True):
                 continue
             if hasattr(obj, "__len__") and (isinstance(obj, Batch) or obj.ndim > 0):
                 lens.append(len(obj))
             else:
                 raise TypeError(f"Object {obj} in {self} has no len()")
-        if len(lens) == 0:
-            # empty batch has the shape of any, like the tensorflow '?' shape.
-            # So it has no length.
-            raise TypeError(f"Object {self} has no len()")
+        if not lens:
+            return 0
         return min(lens)
 
     def is_empty(self, recurse: bool = False) -> bool:
