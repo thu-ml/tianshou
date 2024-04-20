b42ad64f797382bb3b23f98f2b8a8ddbe8a7e592
Michael Panchenko <m.panchenko@appliedai.de>
2024-04-20 23:41:57 +0200

    Launcher: don't modify user input, set loky as default backend

--- tianshou/evaluation/launcher.py
+++ tianshou/evaluation/launcher.py
@@ -1,6 +1,9 @@
+"""Provides a basic interface for launching experiments. The API is experimental and subject to change!."""
+
 import logging
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
+from copy import copy
 from dataclasses import asdict, dataclass
 from enum import Enum
 from typing import Literal
@@ -16,7 +19,7 @@ log = logging.getLogger(__name__)
 class JoblibConfig:
     n_jobs: int = -1
     """The maximum number of concurrently running jobs. If -1, all CPUs are used."""
-    backend: Literal["loky", "multiprocessing", "threading"] | None = None
+    backend: Literal["loky", "multiprocessing", "threading"] | None = "loky"
     """Allows to hard-code backend, otherwise inferred based on prefer and require."""
     verbose: int = 10
     """If greater than zero, prints progress messages."""
@@ -36,9 +39,14 @@ class SequentialExpLauncher(ExpLauncher):
 
 class JoblibExpLauncher(ExpLauncher):
     def __init__(self, joblib_cfg: JoblibConfig | None = None) -> None:
-        self.joblib_cfg = joblib_cfg or JoblibConfig()
+        self.joblib_cfg = copy(joblib_cfg) if joblib_cfg is not None else JoblibConfig()
         # Joblib's backend is hard-coded to loky since the threading backend produces different results
-        self.joblib_cfg.backend = "loky"
+        if self.joblib_cfg.backend != "loky":
+            log.warning(
+                f"Ignoring the user provided joblib backend {self.joblib_cfg.backend} and using loky instead. "
+                f"The current implementation requires loky to work and will be relaxed soon",
+            )
+            self.joblib_cfg.backend = "loky"
 
     def launch(self, experiments: Sequence[Experiment]) -> None:
         Parallel(**asdict(self.joblib_cfg))(delayed(self._safe_execute)(exp) for exp in experiments)
