f8cca8b07c45a2f98036df8b9f359bbfcc3eb43e
Dominik Jain <d.jain@appliedai.de>
2024-04-30 17:22:11 +0200

    Improve creation of multiple seeded experiments:   * Add class ExperimentCollection to improve usability   * Remove parameters from ExperimentBuilder.build   * Renamed ExperimentBuilder.build_default_seeded_experiments to build_seeded_collection,     changing the return type to ExperimentCollection   * Replace temp_config_mutation (which was not appropriate for the public API) with     method copy (which performs a safe deep copy)

--- test/highlevel/test_experiment_builder.py
+++ test/highlevel/test_experiment_builder.py
@@ -79,30 +79,3 @@ def test_experiment_builder_discrete_default_params(builder_cls: type[Experiment
     experiment = builder.build()
     experiment.run(run_name="test")
     print(experiment)
-
-
-def test_temp_builder_modification() -> None:
-    env_factory = DiscreteTestEnvFactory()
-    sampling_config = SamplingConfig(
-        num_epochs=1,
-        step_per_epoch=100,
-        num_train_envs=2,
-        num_test_envs=2,
-    )
-    builder = PPOExperimentBuilder(
-        experiment_config=ExperimentConfig(persistence_enabled=False),
-        env_factory=env_factory,
-        sampling_config=sampling_config,
-    )
-    original_seed = builder.experiment_config.seed
-    original_train_seed = builder.sampling_config.train_seed
-
-    with builder.temp_config_mutation():
-        builder.experiment_config.seed += 12345
-        builder.sampling_config.train_seed += 456
-        exp = builder.build()
-
-    assert builder.experiment_config.seed == original_seed
-    assert builder.sampling_config.train_seed == original_train_seed
-    assert exp.config.seed == original_seed + 12345
-    assert exp.sampling_config.train_seed == original_train_seed + 456
