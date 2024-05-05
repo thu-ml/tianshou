2abb4dac24c41cb58c6c73023ecc6dfeb4c694cd
Michael Panchenko <m.panchenko@appliedai.de>
2024-05-05 22:23:13 +0200

    Reinstated warning module

--- tianshou/utils/__init__.py
+++ tianshou/utils/__init__.py
@@ -6,11 +6,13 @@ from tianshou.utils.logger.wandb import WandbLogger
 from tianshou.utils.lr_scheduler import MultipleLRSchedulers
 from tianshou.utils.progress_bar import DummyTqdm, tqdm_config
 from tianshou.utils.statistics import MovAvg, RunningMeanStd
+from tianshou.utils.warning import deprecation
 
 __all__ = [
     "MovAvg",
     "RunningMeanStd",
     "tqdm_config",
+    "deprecation",
     "DummyTqdm",
     "BaseLogger",
     "TensorboardLogger",
