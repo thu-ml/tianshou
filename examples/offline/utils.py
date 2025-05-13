13b093b752fa369e72da9abc699d737a49c0e98c
Omar Younis <omar.younis98@gmail.com>
2025-05-13 19:06:59 +0200

    minari support tentative

--- examples/offline/utils.py
+++ examples/offline/utils.py
@@ -2,9 +2,51 @@ import d4rl
 import gymnasium as gym
 import h5py
 import numpy as np
+import minari
 
-from tianshou.data import ReplayBuffer
+from tianshou.data import ReplayBuffer, Batch
 from tianshou.utils import RunningMeanStd
+
+
+def load_buffer_minari(dataset_name: str) -> ReplayBuffer:
+    dataset = minari.load_dataset(dataset_name)
+    total_size = dataset.total_steps
+
+    obs = np.empty((total_size, *dataset.observation_space.shape), dtype=dataset.observation_space.dtype)
+    act = np.empty((total_size, *dataset.action_space.shape), dtype=dataset.action_space.dtype)
+    rew = np.empty((total_size,))
+    terminated = np.empty((total_size,))
+    truncated = np.empty((total_size,))
+    obs_next = np.empty((total_size, *dataset.observation_space.shape))
+
+    current_index = 0
+    for episode in dataset:
+        next_index = current_index + len(episode.actions)
+        obs[current_index:next_index] = episode.observations[:-1]
+        act[current_index:next_index] = episode.actions
+        rew[current_index:next_index] = episode.rewards
+        obs_next[current_index:next_index] = episode.observations[1:]
+        terminated[current_index:next_index] = episode.terminations
+        truncated[current_index:next_index] = episode.truncations
+        current_index = next_index
+
+    done_all = np.logical_or(terminated, truncated)
+    replay_buffer = ReplayBuffer(size=total_size)
+    data_batch = Batch(
+        obs=obs,
+        act=act,
+        rew=rew,
+        terminated=terminated,
+        truncated=truncated,
+        done=done_all,
+        obs_next=obs_next,
+    )
+
+    replay_buffer.set_batch(data_batch)
+    replay_buffer._size = total_size
+    replay_buffer._insertion_idx = 0
+    replay_buffer.last_index[0] = (total_size - 1) % total_size
+    return replay_buffer
 
 
 def load_buffer_d4rl(expert_data_task: str) -> ReplayBuffer:
