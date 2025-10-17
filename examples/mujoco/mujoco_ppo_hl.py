9234fcdcb7a9f5b595f3839d298694abcb7adc79
MischaPanch <mischa.panch@gmail.com>
2025-10-17 14:45:39 +0200

    Modified ppo_hl example, simplifying the config options and adding multi-experiment option

--- examples/mujoco/mujoco_ppo_hl.py
+++ examples/mujoco/mujoco_ppo_hl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from collections.abc import Sequence
+import os
 from typing import Literal
 
 import torch
@@ -17,72 +17,66 @@ from tianshou.highlevel.params.lr_scheduler import LRSchedulerFactoryFactoryLine
 
 
 def main(
-    experiment_config: ExperimentConfig,
     task: str = "Ant-v4",
-    buffer_size: int = 4096,
-    hidden_sizes: Sequence[int] = (64, 64),
-    lr: float = 3e-4,
-    gamma: float = 0.99,
-    epoch: int = 100,
-    epoch_num_steps: int = 30000,
-    collection_step_num_env_steps: int = 2048,
-    update_step_num_repetitions: int = 10,
-    batch_size: int = 64,
-    num_train_envs: int = 10,
-    num_test_envs: int = 10,
-    return_scaling: bool = True,
-    vf_coef: float = 0.25,
-    ent_coef: float = 0.0,
-    gae_lambda: float = 0.95,
-    bound_action_method: Literal["clip", "tanh"] | None = "clip",
-    lr_decay: bool = True,
-    max_grad_norm: float = 0.5,
-    eps_clip: float = 0.2,
-    dual_clip: float | None = None,
-    value_clip: bool = False,
-    advantage_normalization: bool = False,
-    recompute_adv: bool = True,
+    persistence_base_dir: str = "log",
+    num_experiments: int = 5,
+    experiment_launcher: Literal["sequential", "joblib"] = "sequential",
 ) -> None:
+    """
+    Train an agent using PPO on a specified MuJoCo task, potentially running multiple experiments with different seeds
+    and evaluating the results using rliable.
+
+    :param task: the MuJoCo task to train on.
+    :param persistence_base_dir: the base directory for logging and saving experiment data,
+        the task name will be appended to it.
+    :param num_experiments: the number of experiments to run. The experiments differ exclusively in the seeds.
+    :param experiment_launcher: the type of experiment launcher to use, only has an effect if `num_experiments>1`.
+        You can use "joblib" for parallel execution of whole experiments.
+    """
+    persistence_base_dir = os.path.abspath(os.path.join(persistence_base_dir, task))
+    experiment_config = ExperimentConfig(persistence_base_dir=persistence_base_dir, watch=False)
+
     training_config = OnPolicyTrainingConfig(
-        max_epochs=epoch,
-        epoch_num_steps=epoch_num_steps,
-        batch_size=batch_size,
-        num_train_envs=num_train_envs,
-        num_test_envs=num_test_envs,
-        buffer_size=buffer_size,
-        collection_step_num_env_steps=collection_step_num_env_steps,
-        update_step_num_repetitions=update_step_num_repetitions,
+        max_epochs=1,
+        epoch_num_steps=5000,
+        batch_size=64,
+        num_train_envs=5,
+        num_test_envs=5,
+        test_step_num_episodes=5,
+        buffer_size=4096,
+        collection_step_num_env_steps=2048,
+        update_step_num_repetitions=1,
     )
 
     env_factory = MujocoEnvFactory(task, obs_norm=True)
 
-    experiment = (
+    hidden_sizes = (64, 64)
+    experiment_builder = (
         PPOExperimentBuilder(env_factory, experiment_config, training_config)
-        .with_name(task)
         .with_ppo_params(
             PPOParams(
-                gamma=gamma,
-                gae_lambda=gae_lambda,
-                action_bound_method=bound_action_method,
-                return_scaling=return_scaling,
-                ent_coef=ent_coef,
-                vf_coef=vf_coef,
-                max_grad_norm=max_grad_norm,
-                value_clip=value_clip,
-                advantage_normalization=advantage_normalization,
-                eps_clip=eps_clip,
-                dual_clip=dual_clip,
-                recompute_advantage=recompute_adv,
-                lr=lr,
-                lr_scheduler=LRSchedulerFactoryFactoryLinear(training_config) if lr_decay else None,
+                gamma=0.99,
+                gae_lambda=0.95,
+                action_bound_method="clip",
+                return_scaling=True,
+                ent_coef=0.0,
+                vf_coef=0.25,
+                max_grad_norm=0.5,
+                value_clip=False,
+                advantage_normalization=False,
+                eps_clip=0.2,
+                dual_clip=None,
+                recompute_advantage=True,
+                lr=3e-4,
+                lr_scheduler=LRSchedulerFactoryFactoryLinear(training_config),
             ),
         )
         .with_actor_factory_default(hidden_sizes, torch.nn.Tanh, continuous_unbounded=True)
         .with_critic_factory_default(hidden_sizes, torch.nn.Tanh)
-        .build()
     )
-    experiment.run()
+
+    experiment_builder.build_and_run(num_experiments=num_experiments, launcher=experiment_launcher)
 
 
 if __name__ == "__main__":
-    logging.run_cli(main)
+    result = logging.run_cli(main, level=logging.INFO)
